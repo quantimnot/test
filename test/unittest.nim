1b70e7302e3462b454882d4a031e7ad4ab2baac9
name <name@example.com>
2021-02-18 15:57:00 -0500

    wip

--- test/unittest.nim
+++ test/unittest.nim
@@ -9,93 +9,220 @@ export stdUnittest.fail]#
 
 import pkg/prelude/[common, lib]
 
-#proc runTests*()
+iface TextMatcher:
+  proc firstMatch(corpus, pattern: string): Option[Slice[int]]
+type TestKind* {.pure.} = enum
+  Unit,
+  Functional,
+  Coverage,
+  Benchmark,
+  Syscall,
+type TestSubject* = object
+  ident*: string
+  signature*: string
+  module*: string
+  modulePath*: string
+  line*: int
+  column*: int
+  sigHash*: string
+  implHash*: string
+type ExitCodeRange* = Slice[int]
+type ExecEnvironment* = object
+  command*: string
+  variables*: seq[(string, string)]
+  workingDir*: string
+type Expectation* = object
+  exitCode*: ExitCodeRange
+  stdout*: seq[TextMatcher]
+  stderr*: seq[TextMatcher]
+type TestCase* = object
+  subject*: TestSubject
+  env*: ExecEnvironment
+  expect*: Expectation
+  title*: string
+  stdin*: string
 
 when defined test:
-  import pkg/coverage
-  export coverage.CovData
+  when defined testCoverage:
+    import pkg/coverage
+    export coverage.CovData
+  when defined testBench:
+    discard
+  when defined testMemLeaks:
+    discard
+  when defined testSyscalls:
+    discard
+  when defined testFileIo:
+    discard
+
   from std/hashes import hash
   from std/os import splitFile
 
-  const TEST_RUN_NIM_CMD {.strdefine.} = "nim r"
+  import std/strutils
+  type StrLitMatcher = ref object of RootRef
+  proc firstMatch(self: StrLitMatcher, corpus, pattern: string): Option[Slice[int]] =
+    let offset = pattern.find(pattern)
+    if offset == -1: return
+    else: some offset..pattern.len
 
-  type TestSubject* = object
-    ident*: string
-    signature*: string
-    module*: string
-    modulePath*: string
-    line*: int
-    column*: int
-    sigHash*: string
-    implHash*: string
-  type TextMatcher* = RootObj
-  type ExitCodeRange* = Slice[int]
-  type Expectation* = object
-    exitCode*: ExitCodeRange
-    stdout*: seq[TextMatcher]
-    stderr*: seq[TextMatcher]
+  import std/pegs
+  type PegMatcher = ref object of RootRef
+  proc firstMatch(self: PegMatcher, corpus, pattern: string): Option[Slice[int]] =
+    var matches: seq[string]
+    when compiles(pattern.findBounds(pattern.peg)):
+      # will be faster without handling matches
+      {.error: "Good news everybody! Change the next source line for a speed boost.".}
+    let (offset, length) = pattern.findBounds(pattern.peg, matches)
+    if offset == -1: return
+    else: some offset..length
+
+  import std/re
+  type RegexMatcher = ref object of RootRef
+  proc firstMatch(self: RegexMatcher, corpus, pattern: string): Option[Slice[int]] =
+    var matches: seq[string]
+    let (offset, length) = pattern.findBounds(pattern.peg, matches)
+    if offset == -1: return
+    else: some offset..length
+
   converter toExitCodeRange*(returnCode: int): ExitCodeRange =
     returnCode..returnCode
 
-  var testCases {.compileTime.}: seq[(TestSubject, string, string, Expectation)]
+  var testCases: seq[TestCase]
+  const
+    nimRunTemplate {.strdefine.} = "nim {backend} {defines} r - 2>{testRunInputHash}"
+    defaultEnvironment = ExecEnvironment(command: nimRunTemplate)
+    defaultExpectation = Expectation()
 
-  #from std/exitprocs import addExitProc
-  #exitprocs.addExitProc runTests
+  proc runTests() =
+    if testCases.len > 0:
+      log "running " & $testCases.len & " tests"
+      for t in testCases:
+        echo repr t
 
-#[   proc addTestCase(symbol: NimNode, executor, source: string, expect = Expectation()) {.compileTime.} =
-    unittest.testCases.add((
-      TestSubject(
-        ident: symbol.strVal,
-        signature: symbol.repr,
-        module: symbol.lineInfoObj.filename.splitFile.name,
-        modulePath: symbol.lineInfoObj.filename,
-        line: symbol.lineInfoObj.line,
-        column: symbol.lineInfoObj.column,
-        #sigHash: symbol.sigHash,
-        #implHash: symbol.implHash
-      ),
-      executor,
-      source,
-      expect
-    )) ]#
+  from std/exitprocs import addExitProc
+  exitprocs.addExitProc runTests
 
-macro runTests*() =
-  when defined test:
-    log "running tests"
-    for t in testCases:
-      echo repr t
-    quit()
-  else: discard
+  macro conanicalSigForm(sym: typed): string =
+    newLit getTypeImpl(sym).repr
+  macro sigHash(sym: typed): untyped =
+    newLit signatureHash sym
+  macro implHash(sym: typed): untyped =
+    newLit symBodyHash sym
 
-proc extractTests(procDef: NimNode): (NimNode, NimNode) {.compileTime.} =
-  ## Extract all first-level `nnkCommand(ident"test", nnkStrLit, nnkStmtList)`
-  ## AST patterns from the proc and return them.
-  result = (procDef.copy, newStmtList())
+  func buildTestCase(title = "", env = defaultEnvironment, expect = defaultExpectation, stdin = ""):
+    TestCase {.compileTime, inline.} = TestCase(title: title, env: env, expect: expect, stdin: stdin)
+
+proc extractTests(procDef: NimNode): (NimNode, seq[TestCase]) {.compileTime.} =
+  result[0] = procDef.copy
   alias origBody, procDef[6]
   alias newBody, result[0][6]
   alias tests, result[1]
   result[0][6] = newStmtList()
   assert procDef.kind in RoutineNodes
   assert origBody.kind == nnkStmtList
+  macro override[T: ExecEnvironment|Expectation](a: T, b: NimNode): T =
+    var c = a.copy
+    c
   for child in origBody.children:
     child.matchAst:
-    of nnkCommand(ident"test", nnkStrLit, nnkStmtList):
-      tests.add child
+    of nnkCall(ident"test", `body` @ nnkStmtList):
+      when defined test: tests.add buildTestCase($tests.len, stdin = body.repr)
+    of nnkCommand(ident"test", `title` @ nnkStrLit, `body` @ nnkStmtList):
+      when defined test: tests.add buildTestCase($title, stdin = body.repr)
+    of nnkCommand(ident"test", nnkCommand(`title` @ nnkStrLit,
+       nnkCommand(ident"with", `envOverride` @ nnkPar)), `body` @ nnkStmtList):
+      when defined test: tests.add buildTestCase($title, override(defaultEnvironment, envOverride), stdin = body.repr)
+    of nnkCommand(ident"test",
+       nnkCommand(ident"with", `envOverride` @ nnkPar), `body` @ nnkStmtList):
+      when defined test: tests.add buildTestCase($tests.len, override(defaultEnvironment, envOverride), stdin = body.repr)
+    of nnkCommand(ident"with", nnkCommand(`envOverride` @ nnkPar,
+       nnkCommand(ident"test", `title` @ nnkStrLit)), `body` @ nnkStmtList):
+      when defined test: tests.add buildTestCase($title, override(defaultEnvironment, envOverride), stdin = body.repr)
+    of nnkCommand(ident"with", `envOverride` @ nnkPar, `body` @ nnkStmtList):
+      when defined test:
+        var env = override(defaultEnvironment, envOverride)
+        body.matchAst:
+        of nnkCall(ident"test", `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($tests.len, env, stdin = body.repr)
+        of nnkCommand(ident"test", `title` @ nnkStrLit, `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($title, env = env, stdin = body.repr)
+        of nnkCommand(ident"test",
+          nnkCommand(ident"expect", `expectOverride` @ nnkPar), `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($tests.len, env, override(defaultExpectation, envOverride), body.repr)
+        of nnkCommand(ident"test", nnkCommand(`title` @ nnkStrLit,
+          nnkCommand(ident"expect", `expectOverride` @ nnkPar)), `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($title, env, override(defaultExpectation, envOverride), body.repr)
+        of nnkCommand(ident"expect", `expectOverride` @ nnkPar, `body` @ nnkStmtList):
+          var expect = override(defaultExpectation, expectOverride)
+          body.matchAst:
+          of nnkCall(ident"test", `body` @ nnkStmtList):
+            when defined test: tests.add buildTestCase($tests.len, env, expect, stdin = body.repr)
+          of nnkCommand(ident"test", `title` @ nnkStrLit, `body` @ nnkStmtList):
+            when defined test: tests.add buildTestCase($title, env, expect, body.repr)
+    of nnkCommand(ident"expect", `expectOverride` @ nnkPar, `body` @ nnkStmtList):
+      when defined test:
+        var expect = override(defaultExpectation, expectOverride)
+        body.matchAst:
+        of nnkCall(ident"test", `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($tests.len, expect = expect, stdin = body.repr)
+        of nnkCommand(ident"test", `title` @ nnkStrLit, `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($title, expect = expect, stdin = body.repr)
+        of nnkCommand(ident"test",
+          nnkCommand(ident"with", `envOverride` @ nnkPar), `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($tests.len, override(defaultEnvironment, envOverride), expect, body.repr)
+        of nnkCommand(ident"test", nnkCommand(`title` @ nnkStrLit,
+          nnkCommand(ident"with", `envOverride` @ nnkPar)), `body` @ nnkStmtList):
+          when defined test: tests.add buildTestCase($title, override(defaultEnvironment, envOverride), expect, body.repr)
+        of nnkCommand(ident"with", `envOverride` @ nnkPar, `body` @ nnkStmtList):
+          var env = override(defaultEnvironment, envOverride)
+          body.matchAst:
+          of nnkCall(ident"test", `body` @ nnkStmtList):
+            when defined test: tests.add buildTestCase($tests.len, env, expect, body.repr)
+          of nnkCommand(ident"test", `title` @ nnkStrLit, `body` @ nnkStmtList):
+            when defined test: tests.add buildTestCase($title, env, expect, body.repr)
+    of nnkCommand(ident"test", nnkCommand(`title` @ nnkStrLit,
+       nnkCommand(ident"expect", `expectOverride` @ nnkPar)), `body` @ nnkStmtList):
+        when defined test: tests.add buildTestCase($title, expect = override(defaultExpectation, expectOverride), stdin = body.repr)
+    of nnkCommand(ident"test",
+       nnkCommand(ident"expect", `expectOverride` @ nnkPar), `body` @ nnkStmtList):
+        when defined test: tests.add buildTestCase($tests.len, expect = override(defaultExpectation, expectOverride), stdin = body.repr)
+    of nnkCommand(ident"test", nnkCommand(`title` @ nnkStrLit,
+       nnkCommand(ident"with", nnkCommand(`envOverride` @ nnkPar,
+       nnkCommand(ident"expect", `expectOverride` @ nnkPar)))), `body` @ nnkStmtList):
+        when defined test: tests.add buildTestCase($title, override(defaultEnvironment, envOverride),
+                   override(defaultExpectation, expectOverride), body.repr)
+    of nnkCommand(ident"test",
+       nnkCommand(ident"with", nnkCommand(`envOverride` @ nnkPar,
+       nnkCommand(ident"expect", `expectOverride` @ nnkPar))), `body` @ nnkStmtList):
+        when defined test: tests.add buildTestCase($tests.len, override(defaultEnvironment, envOverride),
+                   override(defaultExpectation, expectOverride), body.repr)
+    of nnkCommand(ident"test", nnkCommand(`title` @ nnkStrLit,
+       nnkCommand(ident"expect", nnkCommand(`expectOverride` @ nnkPar,
+       nnkCommand(ident"with", `envOverride` @ nnkPar)))), `body` @ nnkStmtList):
+        when defined test: tests.add buildTestCase($title, override(defaultEnvironment, envOverride),
+                   override(defaultExpectation, expectOverride), body.repr)
+    of nnkCommand(ident"with", nnkCommand(`envOverride` @ nnkPar,
+       nnkCommand(ident"test", nnkCommand(`title` @ nnkStrLit,
+       nnkCommand(ident"expect", `expectOverride` @ nnkPar)))), `body` @ nnkStmtList):
+        when defined test: tests.add buildTestCase($title, override(defaultEnvironment, envOverride),
+                   override(defaultExpectation, expectOverride), body.repr)
+    of nnkCommand(ident"expect", nnkCommand(`expectOverride` @ nnkPar,
+       nnkCommand(ident"with", nnkCommand(`envOverride` @ nnkPar,
+       nnkCommand(ident"test", `title` @ nnkStrLit)))), `body` @ nnkStmtList):
+        when defined test: tests.add buildTestCase($title, override(defaultEnvironment, envOverride),
+                   override(defaultExpectation, expectOverride), body.repr)
     else: newBody.add child
-
-proc implHash(n: NimNode): string = n.symBodyHash
 
 macro test*(origProcDef: untyped): untyped =
   result = origProcDef
   matchAst(origProcDef, matchErrors):
   of {nnkProcDef, nnkFuncDef}:
     let (strippedProcDef, tests) = origProcDef.extractTests
-    when defined test:
-      result = buildAst(stmtList):
-        when defined testCoverage:
-          getAst(coverage.cov(strippedProcDef)) # instrument for branch coverage
-        else:
-          strippedProcDef
+    result = buildAst(stmtList):
+      when defined(test) and defined(testCoverage):
+        getAst(coverage.cov(strippedProcDef)) # instrument for branch coverage
+      else: strippedProcDef
+      when defined(test):
         if tests.len > 0:
           let ident: string = $origProcDef.name
           let sig: string = $origProcDef.name.repr
@@ -105,43 +232,37 @@ macro test*(origProcDef: untyped): untyped =
           let macroBaseName = "test" & origProcDef.name.strVal
           let line = symbol.lineInfoObj.line
           let column = symbol.lineInfoObj.column
+          var testIndex: int
           for test in tests:
-            let macroName = ident(macroBaseName & $hash(test[1].strVal))
-            let source = "import " & currentSourcePath() & "\n" & test[2].repr
-            let executor = TEST_RUN_NIM_CMD & " -d:testCoverage -"
-            #unittest.addTestCase(origProcDef.name, command, source)
+            let procName = ident(macroBaseName & '_' & $testIndex)
+            let stdin = """import "./""" & module & """"\n""" & test.stdin
+            let title = test.title
             quote do:
-              macro `macroName` =
-                unittest.testCases.add((
-                  TestSubject(
-                    ident: `ident`,
-                    signature: `sig`,
-                    module: `module`,
-                    modulePath: `modulePath`,
-                    line: `line`,
-                    column: `column`,
-                    sigHash: `symbol`.sigHash,
-                    implHash: `symbol`.implHash
-                  ),
-                  `executor`,
-                  `source`,
-                  Expectation()
+              proc `procName` =
+                unittest.testCases.add(
+                  TestCase(
+                    title: `title`,
+                    subject: TestSubject(
+                      ident: `ident`,
+                      signature: `sig`,
+                      module: `module`,
+                      modulePath: `modulePath`,
+                      line: `line`,
+                      column: `column`,
+                      sigHash: `symbol`.sigHash,
+                      implHash: `symbol`.implHash
+                    ),
+                    env: ExecEnvironment(),
+                    expect: Expectation(),
+                    stdin: `stdin`
                 ))
-                #unittest.addTestCase(`symbol`, `executor`, `source`)
-                #let output, exitCode = gorgeEx(`executor`, `source`, `symbol`.implHash)
-                #echo output
-              `macroName`
-  else:
-    when defined test:
-      echo matchErrors
+              `procName`()
+            testIndex.inc()
+  #else:
+    #when defined test:
+    #  echo matchErrors
+    #else: discard
   log repr result
-
-macro conanicalSigForm(sym: typed): string =
-  newLit getTypeImpl(sym).repr
-macro sigHash(sym: typed): untyped =
-  newLit signatureHash sym
-macro implHash(sym: typed): untyped =
-  newLit symBodyHash sym
 
 proc lessThanTen*[T](x: T, o = ""): bool {.test.} =
   test "lessThanTen":
@@ -150,8 +271,24 @@ proc lessThanTen*[T](x: T, o = ""): bool {.test.} =
       check 5.lessThanTen
       check not 10.lessThanTen
       check not 11.lessThanTen
+  test "other":
+    echo "hello"
+  test with (command: "nim r -"):
+    quit 1
+  with (command: "nim r -") test "lessThanTen":
+    quit 1
+#[   with (command: "nim r -"):
+    test "lessThanTen":
+      quit 1 ]#
+  test "lessThanTen" expect (exitCode: 0..10):
+    quit 1
+  test "lessThanTen" with (command: "nim r -") expect (exitCode: 0..10):
+    quit 1
+  test "lessThanTen" expect (exitCode: 0..10) with (command: "nim r -"):
+    quit 1
+  with (command: "nim r -") test "lessThanTen" expect (exitCode: 0..10):
+    quit 1
   if x < 10:
     return true
   else:
     return false
-runTests()
