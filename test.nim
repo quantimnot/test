caf2629ef0413fc2b2546ebe6da52e8bc795ee1c
quantimnot <quantimnot@users.noreply.github.com>
2022-07-03 17:56:31 -0400

    Add library entry point

--- test.nim
+++ test.nim
@@ -1,1 +1,19 @@
-include test/unittest
+from std/os import splitFile
+
+include test/testable
+
+when defined test:
+  import test/tags
+  import std/unittest
+  export unittest
+
+template test*(codeBlock) =
+  bind splitFile
+  when defined test:
+    when isMainModule or inTestTags([splitFile(currentSourcePath()).name]):
+      {.push hint[GlobalVar]: off.}
+      codeBlock
+      {.pop.}
+
+import isolated_test
+export isolated_test
